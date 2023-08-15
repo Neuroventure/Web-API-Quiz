b553b9ab72084068dd356b87ff8ffa1a7cb22dba
Sean Keller <sean.keller.sk@gmail.com>
2023-08-15 14:10:54 -0700

    added end of game high score checks and if statements

--- script.js
+++ script.js
@@ -183,3 +183,135 @@ var answerCorrect = function() {
      showScore();
       }
 }
+
+//Display total score screen at end of game
+var showScore = function () {
+    containerQuestionEl.classList.add("hide");
+    containerEndEl.classList.remove("hide");
+    containerEndEl.classList.add("show");
+  
+    var scoreDisplay = document.createElement("p");
+    scoreDisplay.innerText = ("Your final score is " + score + "!");
+    containerScoreEl.appendChild(scoreDisplay);
+  }       
+  
+  //create high score values
+  var createHighScore = function(event) { 
+    event.preventDefault() 
+    var initials = document.querySelector("#initials").value;
+    if (!initials) {
+      alert("Enter your intials!");
+      return;
+    }
+  
+  formInitials.reset();
+  
+  var HighScore = {
+  initials: initials,
+  score: score
+  } 
+  
+  //push and sort scores
+  HighScores.push(HighScore);
+  HighScores.sort((a, b) => {return b.score-a.score});
+  
+  //clear visibile list to resort
+  while (listHighScoreEl.firstChild) {
+   listHighScoreEl.removeChild(listHighScoreEl.firstChild)
+  }
+  //create elements in order of high scores
+  for (var i = 0; i < HighScores.length; i++) {
+  var highscoreEl = document.createElement("li");
+  highscoreEl.ClassName = "high-score";
+  highscoreEl.innerHTML = HighScores[i].initials + " - " + HighScores[i].score;
+  listHighScoreEl.appendChild(highscoreEl);
+  }
+  
+  saveHighScore();
+  displayHighScores();
+  
+  }
+  //save high score
+  var saveHighScore = function () {
+    localStorage.setItem("HighScores", JSON.stringify(HighScores))
+        
+  }
+  
+  //load values/ called on page load
+  var loadHighScore = function () {
+    var LoadedHighScores = localStorage.getItem("HighScores")
+        if (!LoadedHighScores) {
+        return false;
+    }
+  
+    LoadedHighScores = JSON.parse(LoadedHighScores);
+    LoadedHighScores.sort((a, b) => {return b.score-a.score})
+  
+  
+    for (var i = 0; i < LoadedHighScores.length; i++) {
+        var highscoreEl = document.createElement("li");
+        highscoreEl.ClassName = "high-score";
+        highscoreEl.innerText = LoadedHighScores[i].initials + " - " + LoadedHighScores[i].score;
+        listHighScoreEl.appendChild(highscoreEl);
+  
+        HighScores.push(LoadedHighScores[i]);
+        
+    }
+  }  
+  
+  //display high score screen from link or when intiials entered
+  var displayHighScores = function() {
+  
+    containerHighScoresEl.classList.remove("hide");
+    containerHighScoresEl.classList.add("show");
+    gameover = "true"
+  
+    if (containerEndEl.className = "show") {
+        containerEndEl.classList.remove("show");
+        containerEndEl.classList.add("hide");
+        }
+    if (containerStartEl.className = "show") {
+        containerStartEl.classList.remove("show");
+        containerStartEl.classList.add("hide");
+        }
+        
+    if (containerQuestionEl.className = "show") {
+        containerQuestionEl.classList.remove("show");
+        containerQuestionEl.classList.add("hide");
+        }
+  
+    if (correctEl.className = "show") {
+        correctEl.classList.remove("show");
+        correctEl.classList.add("hide");
+    }
+  
+    if (wrongEl.className = "show") {
+        wrongEl.classList.remove("show");
+        wrongEl.classList.add("hide");
+        }
+    
+  }
+  //clears high scores
+  var clearScores = function () {
+    HighScores = [];
+  
+    while (listHighScoreEl.firstChild) {
+        listHighScoreEl.removeChild(listHighScoreEl.firstChild);
+    }
+  
+    localStorage.clear(HighScores);
+  
+  } 
+  
+  loadHighScore()
+    
+  //on start click, start game
+  btnStartEl.addEventListener("click", startGame)
+  //on submit button -- enter or click
+  formInitials.addEventListener("submit", createHighScore)
+  //when view high-scores is clicked
+  ViewHighScoreEl.addEventListener("click", displayHighScores)
+  //Go back button
+  btnGoBackEl.addEventListener("click", renderStartPage)
+  //clear scores button
+  btnClearScoresEl.addEventListener("click", clearScores)
